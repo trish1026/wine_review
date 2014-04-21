7ba457200112c1fb87a33206bbc28dda88dfac4f
Patricia.Williams <williams_patricia@bah.com>
2014-04-20 20:24:17 -0400

    homework 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 WineReview::Application.routes.draw do
-  get "/wines" => "wines#index", as:'wines'
-  get "/wines/:id" => "wines#show", as: 'wine'
+  # get "/wines" => "wines#index", as:'wines'
+  # get "/wines/new" => "wines#new", as: 'new_wine'
+  # get "/wines/:id" => "wines#show", as: 'wine'
+  # post "/wines" => "wines#create"
+  # get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
+  # patch "/wines/:id" => "wines#update"
+  # delete "/wines/:id" => "wines#destroy"
+
+  resources :wines
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
