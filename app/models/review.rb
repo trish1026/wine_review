37cabc109c98e576ae5c2a86d54749e33df43085
Patricia_Williams <williams_patricia@bah.com>
2014-04-29 19:58:13 -0400

    more 11

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,14 +2,7 @@ class Review < ActiveRecord::Base
 
   STARS = 1..5
 
-  validates :name, :comment, presence: true
-  
-  validates :comment, 
-            length: { minimum: 15, message: 'should have a bit more!' },
-            unless: 'comment.blank?'
-  
-  validates :stars, inclusion: { in: STARS, 
-  	 message: "must be from #{STARS.first} to #{STARS.last}" }
+  validates :name, :comment, :stars, :comment, presence: true
 
   belongs_to :wine
 
